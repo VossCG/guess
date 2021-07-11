ef393a9d8742301dffef850ff131c5011ac59bcd
VossCG <84518506+VossCG@users.noreply.github.com>
2021-07-11 14:35:14 +0800

    字串資源 轉換中文

--- app/src/main/java/com/voss/guess/MainActivity.kt
+++ app/src/main/java/com/voss/guess/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.voss.guess
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.linear_main.*
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.linear_main)
     }
+
+    fun check(view: View) {
+        println("answer : ${secretNumber.secret}")
+        val enterNumber = ed_guessNumber.text.toString().toInt()
+        var message = getString(R.string.bingo)
+        if (enterNumber > secretNumber.secret) {
+            message = getString(R.string.big)
+        } else if (enterNumber < secretNumber.secret) {
+            message = getString(R.string.small)
+        }
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.message))
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.exit),null)
+            .show()
+//        Toast.makeText(this , message , Toast.LENGTH_LONG).show()
+        if(enterNumber==secretNumber.secret)finish()
+    }
+
 }
